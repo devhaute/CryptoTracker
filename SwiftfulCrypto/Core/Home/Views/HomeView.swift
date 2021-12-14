51f6c5fa77542a93948a10343de37da7cb6fcf54
devhaute <devhaute@gmail.com>
2021-12-15 01:38:21 +0900

    타켓 IOS 버전 14+ 설정, CircleButtonView 구현

---
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Header")
+                Spacer()
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -1,17 +1,13 @@
-//
-//  SwiftfulCryptoApp.swift
-//  SwiftfulCrypto
-//
-//  Created by devhaute on 2021/12/13.
-//
-
 import SwiftUI
 
 @main
 struct SwiftfulCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
