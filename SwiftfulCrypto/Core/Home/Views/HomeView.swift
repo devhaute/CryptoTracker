83daed6371e8dda254854815e7f485915b897885
devhaute <devhaute@gmail.com>
2021-12-15 21:53:11 +0900

    navigation header 구현

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -1,13 +1,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var showPortfolio: Bool = false
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
             
             VStack {
-                Text("Header")
+                homeHeader
+                
                 Spacer()
             }
         }
@@ -20,5 +23,32 @@ struct HomeView_Previews: PreviewProvider {
             HomeView()
                 .navigationBarHidden(true)
         }
+    }
+}
+
+extension HomeView {
+    private var homeHeader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
     }
 }
